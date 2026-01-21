98b9c49f8a202b45444f6f635d2c83552e99cd03
wincura <winchocolate1911@gmail.com>
2026-01-22 00:30:33 +0800

    Added adding and listing functionality using dynamic size ArrayList. Tweaked some of the display messages.

--- src/main/java/InvictaBot.java
+++ src/main/java/InvictaBot.java
@@ -1,13 +1,16 @@
 // CS2103T Individual Project by William Scott Win A0273291A
 
-// Imports IO Package to handle user input
-import java.util.Scanner;
+// Imports to use collections and handle user input
+import java.util.*;
 
 @SuppressWarnings("TextBlockMigration") // Suppress the warning to use text block for logo instead
 
 public class InvictaBot {
+    // Declaring data structures to be used by chatbot
+    private static ArrayList<String> log = new ArrayList<String>();
+
     // Method to display logo
-    public static void logo() {
+    private static void logo() {
         // Logo below generated with the help of an external tool from https://patorjk.com/software/taag/
         String logo =   ".___            .__        __        __________        __\n" +
                 "|   | _______  _|__| _____/  |______ \\______   \\ _____/  |_\n" +
@@ -19,17 +22,17 @@ public class InvictaBot {
     }
 
     // Method to display greeting message
-    public static void greet() {
+    private static void greet() {
         System.out.println("_".repeat(70) + "\n" +
-                "Howdy! I'm InvictaBot!\n" +
-                "What can I do for ya, pal?\n" +
+                "\t" + "Howdy! I'm InvictaBot!\n" +
+                "\t" + "What can I do for ya, pal?\n" +
                 "_".repeat(70) + "\n");
     }
 
     // Method to display goodbye message
-    public static void bye() {
+    private static void bye() {
         System.out.println("_".repeat(70) + "\n" +
-        "Bye bye now! You take care!\n" +
+                "\t" + "Bye bye now! You take care!\n" +
                 "_".repeat(70) + "\n");
     }
 
@@ -46,10 +49,19 @@ public class InvictaBot {
                 bye();
                 // Exit loop
                 break;
+            } else if (userInput.equals("list")) {
+                int count = 0;
+                System.out.println("_".repeat(66));
+                for (String msg : log) {
+                    count += 1;
+                    System.out.println("\t" + count + ". " + msg);
+                }
+                System.out.println("_".repeat(66) + "\n");
             } else {
-                // Print user input message
+                // Display added user input message
+                log.add(userInput);
                 System.out.println("_".repeat(66) + "\n" +
-                        "\t" + userInput + "\n" +
+                        "\t" + "added: " + userInput + "\n" +
                         "_".repeat(66) + "\n");
             }
         }
