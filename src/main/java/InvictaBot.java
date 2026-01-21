c82e988ea6fa7c99aae86891641dc492093dda84
wincura <winchocolate1911@gmail.com>
2026-01-21 23:33:09 +0800

    Added greet method that displays greeting message, separated logo into its own method to have a tidier main method.

--- src/main/java/InvictaBot.java
+++ src/main/java/InvictaBot.java
@@ -2,15 +2,30 @@
 @SuppressWarnings("TextBlockMigration") // Suppress the warning to use text block for logo instead
 
 public class InvictaBot {
-    public static void main(String[] args) {
+    // Method to display logo
+    public static void logo() {
         // Logo below generated with the help of an external tool from https://patorjk.com/software/taag/
         String logo =   ".___            .__        __        __________        __\n" +
-                        "|   | _______  _|__| _____/  |______ \\______   \\ _____/  |_\n" +
-                        "|   |/    \\  \\/ /  |/ ___\\   __\\__  \\ |    |  _//  _ \\   __\\\n" +
-                        "|   |   |  \\   /|  \\  \\___|  |  / __ \\|    |   (  <_> )  |\n"  +
-                        "|___|___|  /\\_/ |__|\\___  >__| (____  /______  /\\____/|__|\n" +
-                        "         \\/             \\/          \\/       \\/\n";
+                "|   | _______  _|__| _____/  |______ \\______   \\ _____/  |_\n" +
+                "|   |/    \\  \\/ /  |/ ___\\   __\\__  \\ |    |  _//  _ \\   __\\\n" +
+                "|   |   |  \\   /|  \\  \\___|  |  / __ \\|    |   (  <_> )  |\n"  +
+                "|___|___|  /\\_/ |__|\\___  >__| (____  /______  /\\____/|__|\n" +
+                "         \\/             \\/          \\/       \\/\n";
         System.out.println("Hello from\n" + logo);
+    }
 
+    // Method to display greeting message
+    public static void greet() {
+        System.out.println("_".repeat(70) + "\n" +
+                "Hello! I'm InvictaBot!\n" +
+                "What can I do for you?\n" +
+                "_".repeat(70) + "\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "_".repeat(70) + "\n");
+    }
+
+    public static void main(String[] args) {
+        logo();
+        greet();
     }
 }
